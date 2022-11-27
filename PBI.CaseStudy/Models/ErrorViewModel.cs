1f86c0e6e9a916e7a4296e10c98aea2709bed2cc
Dragan Bunić <dragan.bunic@axelyos.com>
2022-11-27 16:43:20 +0100

    Add project files.

---
+++ PBI.CaseStudy/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PBI.CaseStudy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
