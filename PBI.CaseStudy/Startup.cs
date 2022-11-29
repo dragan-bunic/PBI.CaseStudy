85d98ef59973f8a54c9b79ffc3d1e3f2eb6a13a0
Dragan Bunić <dragan.bunic@axelyos.com>
2022-11-29 21:31:17 +0100

    DevExpress files

--- PBI.CaseStudy/Startup.cs
+++ PBI.CaseStudy/Startup.cs
@@ -27,7 +27,7 @@ namespace PBI.CaseStudy
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
 
             services.AddScoped<IHistoricalData<SecurityHistoricData>, SecurityHistoricalData>();
         }
